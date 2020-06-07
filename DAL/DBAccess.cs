e50de0bf97cf55ae03dce473f0bcf93364cb5a9c
GeorgeYanchynskyj <yurij.yanchynskyj@gmail.com>
2020-06-07 21:52:17 +0300

    add DAL

---
+++ DAL/DBAccess.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DBAccess
+    {
+        private static string _connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Movies;Integrated Security=True";
+
+        public static IDbConnection GetDbConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
